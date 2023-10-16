7a67fb7d7a677d774377afc6cb3a1eed5be08ae5
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-10-16 07:48:56 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,27 @@
 def main():
+    data = generate_random_data()
+
+
+def generate_random_data():
         print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
 
 if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-
 
     return data
 import random
+    for item in data:
+    main()        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+
+import random
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
 
     main()
-    data = generate_random_data()
+if __name__ == "__main__":
+def main():
+    for item in data:
+
 def generate_random_data():
